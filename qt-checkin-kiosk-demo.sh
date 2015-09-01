1dc24d8f3329d340e5152a7d820ea31c961493a2
Giovanni Bauermeister Santana <giovanni.bauermeister@outlook.com>
2015-09-01 10:01:21 -0300

    Update qt-checkin-kiosk-demo.sh
    updated launch script

--- qt-checkin-kiosk-demo.sh
+++ qt-checkin-kiosk-demo.sh
@@ -5,4 +5,4 @@ export QT_QPA_EGLFS_HIDECURSOR=1
 export QT_QPA_EGLFS_FB=/dev/fb0
 flightKiosk -platform eglfs -plugin tslib:/dev/input/event0 &
 export QT_QPA_EGLFS_FB=/dev/fb2
-flighInfo -platform eglfs -plugin tslib:/dev/input/event0
+flighInfo -platform eglfs
