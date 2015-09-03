e0ea70140c4b4e884df8717a0a86909aee33a48b
Giovanni Bauermeister Santana <giovanni.bauermeister@outlook.com>
2015-09-03 14:13:27 -0300

    Create qt-checkin-kiosk-demo-twilio.sh
    add script with twilio

---
+++ qt-checkin-kiosk-demo-twilio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ts_calibrate
+export QT_QPA_EGLFS_DISABLE_INPUT=1
+export QT_QPA_EGLFS_HIDECURSOR=1
+export QT_QPA_EGLFS_FB=/dev/fb0
+flightKiosk -platform eglfs -plugin tslib:/dev/input/event0 &
+export QT_QPA_EGLFS_FB=/dev/fb2
+flighInfo -platform eglfs &
+twilioTest.py "Departure time: 9:50AM"
